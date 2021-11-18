15872e3bd32390239484ba0d10be7d95d33f8974
RajeshPatel17 <rajesh.patel20110@gmail.com>
2021-11-18 14:04:11 -0600

    finished compute header and file

---
+++ compute.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <queue>
+#include "scheduler.h"
+using namespace std;
+
+vector<double> ComputeStatistics(queue<task> finishedTaskArray){
+    double waitTime = 0;
+    double responseTime = 0;
+    double turnaroundTime = 0;
+    double totalCpuTime = 0;
+    double cpuUsage = 0;
+    int finishTime = 0;
+    int tasks = finishedTaskArray.size();
+    while(!finishedTaskArray.empty()) {
+        turnaroundTime += finishedTaskArray.front().endTime - finishedTaskArray.front().arrivalTime;
+
+        waitTime += finishedTaskArray.front().endTime - finishedTaskArray.front().arrivalTime - finishedTaskArray.front().cpuTime;
+
+        totalCpuTime += finishedTaskArray.front().cpuTime;
+
+        responseTime += finishedTaskArray.front().startTime - finishedTaskArray.front().arrivalTime;
+
+        finishTime = max(finishTime, (int)finishedTaskArray.front().endTime);
+
+        finishedTaskArray.pop();
+    }
+    vector<double> stats;
+
+    stats.push_back(waitTime/tasks);
+    stats.push_back(responseTime/tasks);
+    stats.push_back(turnaroundTime/tasks);
+    stats.push_back(totalCpuTime/(double)finishTime);
+
+
+    return stats;
+}
---
+++ compute.h
@@ -0,0 +1,6 @@
+#ifndef _COMPUTE_H
+#define _COMPUTE_H
+
+vector<double> ComputeStatistics(queue<task> finishedTaskArray);
+
+#endif
