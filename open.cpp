36d46a956d0d185452e96af9139af133e563d4c8
RajeshPatel17 <rajesh.patel20110@gmail.com>
2021-11-18 14:12:09 -0600

    wrote open header and main

---
+++ open.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE open(char *filename){
+    FILE *file;
+    //fileName = argv[1];
+    if (! (file = fopen(filename, "r"))) {
+        printf("File %s can't be opened. Please retry ...\n", filename);
+        return;
+    }
+    printf("Open file: %s\n", filename);
+    return *file;
+}
---
+++ open.h
@@ -0,0 +1,6 @@
+#ifndef _OPEN_H
+#define _OPEN_H
+
+FILE open(char *filename);
+
+#endif
