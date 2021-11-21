5ad2fb59679fd16f5338d5cc6b843c226e37ce7f
RajeshPatel17 <rajesh.patel20110@gmail.com>
2021-11-21 13:56:51 -0600

    added makefile and formatted print stats

--- print.cpp
+++ print.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 void DisplayStatistics(vector<double> stats) {
     printf("\n");
-    printf("average wait time: %f\n", stats.at(0));
-    printf("average response time: %f\n", stats.at(1));
-    printf("average turnaround time: %f\n", stats.at(2));
-    printf("total cpu usage: %f\n", stats.at(3));
+    printf("average wait time: %.2f\n", stats.at(0));
+    printf("average response time: %.2f\n", stats.at(1));
+    printf("average turnaround time: %.2f\n", stats.at(2));
+    printf("total cpu usage: %.2f % \n", stats.at(3)*100);
 }
